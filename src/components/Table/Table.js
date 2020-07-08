a9cb671f3246b34ed61577960f84b07c0f6286f0
Noam Israeli <noamisraeli97@gmail.com>
2020-07-08 16:15:12 +0300

    Change table component to functional component

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -4,41 +4,39 @@ import './Table.css'
 import { getEmptyTable } from '../../utils';
 
 
-class Table extends React.Component {
-    render() {
-        const {
-            size = 300, 
-            state = getEmptyTable(), 
-            className = "board", 
-            onCellClick = () => null,
-            withOverlay = false
-        } = this.props;
-        const overlay = withOverlay ? <div className="overlay"></div> : null;
+const Table = (props) => {
+    const {
+        size = 300, 
+        state = getEmptyTable(), 
+        className = "board", 
+        onCellClick = () => null,
+        withOverlay = false
+    } = props;
+    const overlay = withOverlay ? <div className="overlay"></div> : null;
 
-        return (
-            <div className="table-container">
-                {overlay}
-                <table className={className}>
-                    <tbody>
-                        {state.map((row, rowIndex) => {
-                            return (
-                                <tr key={rowIndex}>
-                                    {row.map((cell, i) => {
-                                        return <Cell 
-                                                    key={i} 
-                                                    className={cell.className} 
-                                                    value={cell.value}
-                                                    size={size / row.length}
-                                                    onCellClick={() => onCellClick(i, rowIndex)} />;
-                                    })}
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+    return (
+        <div className="table-container">
+            {overlay}
+            <table className={className}>
+                <tbody>
+                    {state.map((row, rowIndex) => {
+                        return (
+                            <tr key={rowIndex}>
+                                {row.map((cell, i) => {
+                                    return <Cell 
+                                                key={i} 
+                                                className={cell.className} 
+                                                value={cell.value}
+                                                size={size / row.length}
+                                                onCellClick={() => onCellClick(i, rowIndex)} />;
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Table;
