0567291603f72c55001ed87c3e86fc26e239f4c2
Noam Israeli <noamisraeli97@gmail.com>
2020-07-08 14:05:06 +0300

    Fix medium spelling

--- src/utils.js
+++ src/utils.js
@@ -9,7 +9,7 @@ export function createTableFromTurns(turns) {
 export function getEmptyTable() {
     return [
         [{value: "", className: "cell left top"}, {value: "", className: "cell center top"}, {value: "", className: "cell right top"}],
-        [{value: "", className: "cell left midium"}, {value: "", className: "cell center midium"}, {value: "", className: "cell right midium"}],
+        [{value: "", className: "cell left medium"}, {value: "", className: "cell center medium"}, {value: "", className: "cell right medium"}],
         [{value: "", className: "cell left bottom"}, {value: "", className: "cell center bottom"}, {value: "", className: "cell right bottom"}],
     ]
 }
