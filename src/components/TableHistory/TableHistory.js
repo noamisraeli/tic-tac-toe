7328c2c1ba6b6759a9ad24c3dece5cc4223ffca7
Noam Israeli <noamisraeli97@gmail.com>
2020-07-09 10:23:26 +0300

    Change history table component into functional component

--- src/components/TableHistory/TableHistory.js
+++ src/components/TableHistory/TableHistory.js
@@ -1,37 +1,32 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Table from '../Table/Table';
 import './TableHistory.css';
 import { createTableFromTurns } from '../../utils';
 
-class TableHistory extends Component {
-    render() {
-        const {
-            turns,
-            onSnapshotClick
-        } = this.props;
-        return (
-            <div className="table-history-container">
-                <h2>Game history:</h2>
-                    
-                <div className="table-history">
-                    {turns.map((_, index) => {
-                        const tableSnapshot = createTableFromTurns(turns.slice(0, index + 1))
-                        return (
-                            <div 
-                                key={index}
-                                onClick={() => onSnapshotClick(index)}
-                                style={{display: "inline-block"}}> 
-                                {index + 1}
-                                <Table    
-                                    className="snapshot"
-                                    state={tableSnapshot}
-                                    size={100} />
-                            </div>)
-                    })}
-                </div>
+const TableHistory = ({turns, onSnapshotClick}) => {
+    return (
+        <div className="table-history-container">
+            <h2>Game history:</h2>
+                
+            <div className="table-history">
+                {turns.map((_, index) => {
+                    const tableSnapshot = createTableFromTurns(turns.slice(0, index + 1))
+                    return (
+                        <div 
+                            key={index}
+                            onClick={() => onSnapshotClick(index)}
+                            style={{display: "inline-block"}}> 
+                            {index + 1}
+                            <Table    
+                                className="snapshot"
+                                state={tableSnapshot}
+                                size={100} />
+                        </div>)
+                })}
             </div>
-        )
-    }
+        </div>
+    )
 }
+
 
 export default TableHistory;
