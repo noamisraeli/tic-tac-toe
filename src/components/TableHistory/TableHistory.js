ef98764679746eea151c2bdc27370144d43a0691
Noam Israeli <noamisraeli97@gmail.com>
2020-07-09 11:35:34 +0300

    Change parameters of on snapshot click

--- src/components/TableHistory/TableHistory.js
+++ src/components/TableHistory/TableHistory.js
@@ -4,17 +4,19 @@ import './TableHistory.css';
 import { createTableFromTurns } from '../../utils';
 
 const TableHistory = ({turns, onSnapshotClick}) => {
+    console.log(turns);
     return (
         <div className="table-history-container">
             <h2>Game history:</h2>
                 
             <div className="table-history">
                 {turns.map((_, index) => {
-                    const tableSnapshot = createTableFromTurns(turns.slice(0, index + 1))
+                    const snapshotTurns = turns.slice(0, index + 1) 
+                    const tableSnapshot = createTableFromTurns(snapshotTurns)
                     return (
                         <div 
                             key={index}
-                            onClick={() => onSnapshotClick(index)}
+                            onClick={() => onSnapshotClick(tableSnapshot, snapshotTurns)}
                             style={{display: "inline-block"}}> 
                             {index + 1}
                             <Table    
